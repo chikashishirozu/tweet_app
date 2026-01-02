6832bb651ef9c309fd167991657e89c78b7c99cb
CHIKASHI SHIROZU <102590095+chikashishirozu@users.noreply.github.com>
2026-01-02 11:36:27 +0900

    bug fix
    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :patients
-  resources :posts
-  resources :destroy  
+  resources :posts do
+    collection do
+      get 'test'
+    end
+  end
+  # resources :destroy
+  # routes.rb
+  delete 'posts/:id', to: 'posts#destroy', as: 'destroy_post'
   root 'home#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tweets
