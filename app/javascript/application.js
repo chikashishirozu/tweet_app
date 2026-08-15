2f52029b7d2e8f004cd7a829a281ce6e7c2465b9
CHIKASHI SHIROZU <102590095+chikashishirozu@users.noreply.github.com>
2026-08-15 14:01:46 +0900

    Add files via upload

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,19 @@
 // app/javascript/application.js
 
 import "@hotwired/turbo-rails"
+// import "@rails/ujs"
 import "controllers"
-import { Application } from "@hotwired/stimulus";
-import Rails from '@rails/ujs';
-import $ from 'jquery'; // jQueryをインポート
+// import { Application } from "@hotwired/stimulus";
+// import Rails from '@rails/ujs';
+// import $ from 'jquery'; // jQueryをインポート
 
-Rails.start(); // Rails UJSの開始
+// Rails.start(); // Rails UJSの開始
 
 // Stimulusの設定
-const application = Application.start();
-application.debug = false;
+// const application = Application.start();
+application.debug = true;
 window.Stimulus = application;
 
-export { application };
+// export { application };
 
 
