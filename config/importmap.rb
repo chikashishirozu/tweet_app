3adf6fec7f20e61e86d5cba146a76db8a8dcfadf
CHIKASHI SHIROZU <102590095+chikashishirozu@users.noreply.github.com>
2026-08-15 14:07:19 +0900

    Add files via upload

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,9 +4,9 @@ pin "application", preload: true
 
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "jquery" # @3.7.1
-pin "@rails/ujs", to: "https://cdn.jsdelivr.net/npm/@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
-
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.16
+# pin "jquery" # @3.7.1
+# pin "@rails/ujs", to: "https://cdn.jsdelivr.net/npm/@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+# pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
+# pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
