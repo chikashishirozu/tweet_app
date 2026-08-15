0dadc39e6c5f8c5ff7e71eb3dc82420fbf00ef89
CHIKASHI SHIROZU <102590095+chikashishirozu@users.noreply.github.com>
2026-08-15 13:56:37 +0900

    Add files via upload

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,13 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link application.css
+//= link theme.scss
+//= link home.css
+//= link_tree ../stylesheets/posts .css
+
+// importmap-rails が読み込むJSファイル群を precompile 対象に追加
+//= link_tree ../../javascript .js
+
+//= link jquery/dist/jquery.js
+
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
